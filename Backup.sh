5fb4df9776ca286fb7e3fea3a7a427d632bffda6
NeNpO <nenpo@hotmail.com>
2016-11-10 02:44:10 +0200

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -6,24 +6,34 @@ echo "you can send an email to me. Maybe I even will answer."
 echo "My e-mail Adress : nenpo@hotmail.com"
 echo "I am thankful for every Feedback."
 echo ""
-echo "Copying files in Backup folder into the current folder."
+echo "Do you want to copy files in Backup folder into the current folder."
+echo "Your answer y / n :"
+read answer
 
-mkdir BackUpConfFiles
-cd BackUpConfFiles
-cp -Rv /etc/modules-load.d .
-cp -Rv /etc/modprobe.d .
-mkdir ./cron
-cp -Rv /etc/cron.* ./cron
-cp -Rv /etc/samba .
-cp -Rv /etc/conf.d .
-cp -Rv /etc/X11 .
-cp -Rv /boot ./boot
-mkdir ./conky
-cp -Rv /home/$USER/.conky/* ./conky
-cp -v /etc/sddm.conf .
-cp -v /etc/fstab .
-cp -v /etc/mkinitcpio.conf .
-cp -v /etc/pacman.conf .
-cp -v /etc/bash.bashrc .
-cp -v /etc/motd .
-echo "Everything is Done!"
+if [ $answer = n ]; then
+	echo "Thank you. Bye!"
+	exit
+fi
+
+if [ $answer = y ]; then 
+  echo "Okay lets start!"
+  mkdir BackUpConfFiles
+  cd BackUpConfFiles
+  cp -Rv /etc/modules-load.d .
+  cp -Rv /etc/modprobe.d .
+  mkdir ./cron
+  cp -Rv /etc/cron.* ./cron
+  cp -Rv /etc/samba .
+  cp -Rv /etc/conf.d .
+  cp -Rv /etc/X11 .
+  cp -Rv /boot ./boot
+  mkdir ./conky
+  cp -Rv /home/$USER/.conky/* ./conky
+  cp -v /etc/sddm.conf .
+  cp -v /etc/fstab .
+  cp -v /etc/mkinitcpio.conf .
+  cp -v /etc/pacman.conf .
+  cp -v /etc/bash.bashrc .
+  cp -v /etc/motd .
+  echo "Everything is Done!"
+fi
